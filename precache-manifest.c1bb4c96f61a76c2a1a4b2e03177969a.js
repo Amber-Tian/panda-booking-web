cd274de7d12ca4e97738f42d042ab142ea9c581b
Amber <649458276@qq.com>
2020-08-23 12:44:22 +0800

    deploy

---
+++ precache-manifest.c1bb4c96f61a76c2a1a4b2e03177969a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d9a711cc223fef0640",
+    "url": "/panda-booking-web/css/app.76188b39.css"
+  },
+  {
+    "revision": "5342801788e4b29c15a1459b0c88b72b",
+    "url": "/panda-booking-web/index.html"
+  },
+  {
+    "revision": "d6d9a711cc223fef0640",
+    "url": "/panda-booking-web/js/app.b5d3aaa9.js"
+  },
+  {
+    "revision": "2b13858eae489767537d",
+    "url": "/panda-booking-web/js/chunk-vendors.4ccf2424.js"
+  },
+  {
+    "revision": "be52497c3a8618df69aad59992b4ce12",
+    "url": "/panda-booking-web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/panda-booking-web/robots.txt"
+  }
+]);
